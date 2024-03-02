63cddcb12cf7cefbd6741743ca69ba88122bf18e
Shivam Kumar <108357225+Shivam-kumar17@users.noreply.github.com>
2024-03-02 18:26:57 +0530

    Add files via upload

---
+++ ShivamRestaurants/backends/confirm.php
@@ -0,0 +1,40 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "entryform";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$email = $_POST["email"];
+$password = $_POST["password"];
+
+if(!empty($email) && !empty($password) && !is_numeric($email))
+{
+    $query = "select * from data where email = '$email' limit 1";
+    $result = mysqli_query($conn, $query);
+    
+    if($result)
+    {
+        if($result && mysqli_num_rows($result) > 0)
+        {
+            $user_data = mysqli_fetch_assoc($result);
+            if($user_data['password'] == $password)
+            {
+                header("location: ../ShivamRestaurants/index.php");
+                die;
+            }
+        }
+        // echo "<script type='text/javascript'> alert('Login failed: Invalid credentials')</script>";
+        header("location: index.php");
+    }
+    else{
+        // echo "<script type='text/javascript'> alert('Login failed: Invalid credentials')</script>";
+        header("location: index.php");
+    }
+}
+?>
